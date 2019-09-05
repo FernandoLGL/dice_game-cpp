997733d220fd8e198d9bbaf5c81e9b8d33b946f5
Fernando Rosendo <franplt629@gmail.com>
2019-09-05 16:30:29 -0300

    Maybe just a togglePlayer() function remaining. But everything is working fine on Linux

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,9 @@
-// TODO: Main switching players bug fix
+// comment out system("clear") if you want it safe (and working on Windows)
 #include <cstdint>   //to access fixed-width integers
-#include <cstdlib>   //to access rand and srand
+#include <cstdlib>   //to access rand, srand and system("clear")
 #include <ctime>     //for "time" function
 #include <iostream>  //for cin and cout
+#include <string>    //for std::string
 
 // Not using OOP Classes because I'm using this project to test my skills using
 // structs and enums
@@ -24,21 +25,13 @@ struct Player {
 };
 
 /**
- * A function that toggles the player. If the current player is Player ONE, then
- * it switches to Player TWO.
- *
- * @param player current player. Doing this because non-const global variables
- * are evil.
- */
-// void togglePlayer(Player turnPlayer); //pointers or reference needed
-/**
  * A function that gets the PlayerNumber value as a string (instead of values
  * ONE and TWO, we get "ONE" and "TWO")
  *
  * @param player the player to get the number from
  * @return the player number as a string
  */
-std::string getPlayerNumber(Player player);
+std::string getPlayerNumber(Player *player);
 /**
  * Function that prints the welcome message on the screen
  */
@@ -48,37 +41,43 @@ void printWelcome();
  *
  * @param player the player to print information from
  */
-void printPlayerInfo(Player player);
+void printPlayerInfo(Player *player);
 
 int main() {
         // random seed according to system time
         std::srand(std::time(NULL));
 
+        system("clear");
+
         // There is no need for an initial die value, so I'm just using a zero
         // initialization here
         int dieValue{};
         // Variable that will hold the choice for the round (Throw or Stand)
-        int choice{0};
+        int choice{};
         // Initializing both players
         Player player1{PlayerNumber::ONE};
         Player player2{PlayerNumber::TWO};
 
         // Player with the turn. At the beginning it is Player ONE
-        Player turnPlayer{player1};
+        Player *turnPlayer{&player1};
 
         printWelcome();
 
         // The end condition of the while loop is if someone gets the total
         // score to be 100 or higher (which means "continue with the loop until
         // there is a winner")
-        while (player1.totalScore < 100 || player2.totalScore < 100) {
+        while (true) {
                 // just an extra newline here for more vertical space
                 std::cout << '\n';
 
                 printPlayerInfo(turnPlayer);
 
-                std::cout << "\n1 - Throw\n2 - Stand\n";
+                std::cout << "\n1 - Throw\n2 - Stand (new total score will be "
+                          << turnPlayer->totalScore + turnPlayer->partialScore
+                          << ")\n";
                 std::cin >> choice;
+
+                system("clear");
 
                 if (choice == 1) {
                         // once the user chooses to throw a dice, the die value
@@ -88,19 +87,29 @@ int main() {
                         std::cout << "The value shown is " << dieValue << '\n';
 
                         if (dieValue == 1) {
-                                turnPlayer.partialScore = 0;
+                                turnPlayer->partialScore = 0;
                                 std::cout << "Skipping turn\n";
                                 // Now all we have to do is switch players
                                 // replace this with togglePlayer(turnPlayer);
                                 // once it is ready to be used
-                                if (turnPlayer.number == PlayerNumber::ONE)
-                                        turnPlayer = player2;
+                                if (turnPlayer->number == PlayerNumber::ONE)
+                                        turnPlayer = &player2;
                                 else
-                                        turnPlayer = player1;
+                                        turnPlayer = &player1;
                         } else {
+                                // We could do this here, so that the player
+                                // doesn't need to decide to Stand anymore,
+                                // because he already won. But I decided to not
+                                // do this, so that players close to winning can
+                                // choose to taunt the other player.
+                                //
+                                // if ( (turnPlayer->partialScore + dieValue +
+                                // turnPlayer->totalScore) >= 100)
+                                //       break;
+
                                 // once the die has been thrown and its value
                                 // isn't 1, add its value to the partial score
-                                turnPlayer.partialScore += dieValue;
+                                turnPlayer->partialScore += dieValue;
                                 std::cout << "Added to partial score\n";
                         }
                 }
@@ -108,28 +117,54 @@ int main() {
                 else {
                         // if the player stands, then he gets all the score he
                         // gathered by now
-                        turnPlayer.totalScore += turnPlayer.partialScore;
-                        // and of course after doing this we have to reset his partial
-                        // score
-                        turnPlayer.partialScore = 0;
+                        turnPlayer->totalScore += turnPlayer->partialScore;
+
+                        // if it happens that the player's score is now >= 100,
+                        // there is already a winner. Nothing more needed to be
+                        // done here.
+                        if (turnPlayer->totalScore >= 100) break;
+                        // and of course after doing this we have to reset his
+                        // partial score
+                        turnPlayer->partialScore = 0;
 
                         // Now all we have to do is switch turnPlayers
-                        std::cout << "Switching players\n";
+                        std::cout << "Switching players\n\n";
                         // replace this with togglePlayer(turnPlayer); once it
                         // is ready to be used
-                        if (turnPlayer.number == PlayerNumber::ONE)
-                                turnPlayer = player2;
+                        if (turnPlayer->number == PlayerNumber::ONE)
+                                turnPlayer = &player2;
                         else
-                                turnPlayer = player1;
+                                turnPlayer = &player1;
                 }
         }
+
+        // announcing the winner
+        std::cout << "Player " << getPlayerNumber(turnPlayer) << " wins with "
+                  << turnPlayer->totalScore << " points!\n";
+
+        // temporary variable to store the winner's score to compare it tho the
+        // loser's.
+        int tmp{turnPlayer->totalScore};
+
+        // replace this with togglePlayer(turnPlayer); once it
+        // is ready to be used
+        if (turnPlayer->number == PlayerNumber::ONE)
+                turnPlayer = &player2;
+        else
+                turnPlayer = &player1;
+
+        // announcing the loser and the score difference from both players.
+        std::cout << "Player " << getPlayerNumber(turnPlayer) << " lost with "
+                  << turnPlayer->totalScore << " points. A difference of "
+                  << tmp - turnPlayer->totalScore
+                  << " points from the winner!\n";
 
         return 0;
 }
 
-std::string getPlayerNumber(Player player) {
+std::string getPlayerNumber(Player *player) {
         // Return the appropriate string. If it isn't found, return "UNDEFINED"
-        switch (player.number) {
+        switch (player->number) {
                 case PlayerNumber::ONE:
                         return "ONE";
                 case PlayerNumber::TWO:
@@ -139,23 +174,9 @@ std::string getPlayerNumber(Player player) {
         }
 }
 
-#if 0
-void togglePlayer(Player player) {
-        // Switches the player types (if it is TWO, then it becomes ONE and vice
-        // versa) by using a Switch statement
-        switch (player.number) {
-                case PlayerNumber::ONE:
-                        player.number = PlayerNumber::TWO;
-                        return;
-                case PlayerNumber::TWO:
-                        player.number = PlayerNumber::ONE;
-                        return;
-        }
-}
-#endif
 void printWelcome() { std::cout << "\tDice Game\n\n"; }
-void printPlayerInfo(Player player) {
-        std::cout << "player" << getPlayerNumber(player)
-                  << " (Total: " << player.totalScore
-                  << "; Partial: " << player.partialScore << ")";
+void printPlayerInfo(Player *player) {
+        std::cout << "Player " << getPlayerNumber(player)
+                  << " (Total: " << player->totalScore
+                  << "; Partial: " << player->partialScore << ")";
 }
